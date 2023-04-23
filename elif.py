fd6626ece0d51e55203b541591fc22ff205add48
Varshaborkhatariya <vhnandaniya9@gmail.com>
2023-04-23 15:44:21 +0530

    ifelse

---
+++ elif.py
@@ -0,0 +1,14 @@
+""
+a=int(input(" enter a:"))
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+
+if a>b:
+    if a>c:
+        print("a is greater number")
+    else:
+        print("c is greater number")
+elif b>c:
+     print("b is greater number")
+else:
+    print("c is greater number")
