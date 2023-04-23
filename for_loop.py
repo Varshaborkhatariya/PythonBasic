d961dcde747d767e907bdc6e5fe25c5d1f4d7c52
varshaborkhatariya9 <vhnandaniya9@gmail.com>
2023-04-23 16:52:53 +0530

    first

---
+++ for_loop.py
@@ -0,0 +1,24 @@
+for i in range(1,10):
+    print(i)
+print("****************")
+for i in range(2,10):
+    print(i)
+print("***************")
+for i in range(1,10,3):
+    print(i)
+print("*************")
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(1,10):
+    print("*"*i)
+for i in range(1,10):
+    print(" "*(9-i),"*"*i)
+for i in range(1,10):
+    print(" "*(9-i)," *" *i)
+
+
+
+
+
+
---
+++ odd_even number.py
@@ -0,0 +1,14 @@
+a=int(input("enter no:"))
+if a%2==0:
+    print("a,odd")
+else:
+    print("a,even")
+    
+b=int(input("enter no:"))
+if a%2==0:
+    print("b,odd")
+else:
+    print("b,even")
+    
+    
+      
