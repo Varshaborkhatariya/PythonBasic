a62668b89d7a7e808dbc1743181eab59e2f4c9bd
varshaborkhatariya9 <vhnandaniya9@gmail.com>
2023-04-25 13:54:27 +0530

    third

---
+++ StringSlice.py
@@ -0,0 +1,67 @@
+#1234567890123456  +
+s="Tops Technologies"
+#76543210987654321 -
+
+print(s[3:13])
+print(s[:15])
+print(s[5:])
+print(s[2:15:3])
+print(s[::5])
+
+print(s[-15:-7])
+print(s[:-2])
+print(s[-17:])
+print(s[-15:-3:4])
+print(s[::-3])
+print()
+
+s="Python Programing"
+
+print(s[3:13])
+print(s[:15])
+print(s[5:])
+print(s[2:15:3])
+print(s[::5])
+
+print(s[-15:-7])
+print(s[:-2])
+print(s[-17:])
+print(s[-15:-3:4])
+print(s[::-3])
+print()
+
+s="ABC DE FGHIJK LM"
+
+print(s[3:13])
+print(s[:15])
+print(s[5:])
+print(s[2:15:3])
+print(s[::5])
+
+print(s[-15:-7])
+print(s[:-2])
+print(s[-17:])
+print(s[-15:-3:4])
+print(s[::-3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
