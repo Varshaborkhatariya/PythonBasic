7735ad4d35bb948c4ee5d0eca8c0a2b1dd810585
varshaborkhatariya9 <vhnandaniya9@gmail.com>
2023-04-27 16:18:42 +0530

    four

---
+++ ListSlice.py
@@ -0,0 +1,19 @@
+l=[1,2,3.4,"varsha",4.10,10,11,"haresh", 67.90, 8, 90, 20]
+
+print(l[11:])
+print(l[1:4:2])
+print(l[1:4])
+print(l[::8])
+print(l[1:5:1])
+print(l[:5:3])
+print()
+
+print(l[-11])
+print(l[-8:-2:1])
+print(l[-7:-2])
+print(l[::-8])
+print(l[-10:-3:1])
+print(l[:-1:5])
+
+
+
---
+++ StringPractical.py
@@ -0,0 +1,20 @@
+s=input("Enter String :")
+space=0
+digit=0
+alpha=0
+special=0
+
+for i in s:
+    if i.isspace():
+        space=space+1
+    elif i.isdigit():
+        digit+=1
+    elif i.isalpha():
+        alpha+=1
+    else:
+        speacial+=1
+print("Total Alphabets :",alpha)
+print("Total Digit :",digit)
+print("Total Space :",space)
+print("Total special charecter :",special)
+ 
