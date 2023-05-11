24b58ebf6d710f79c682a95038fe9f9abab29904
varshaborkhatariya <vhnandanniaya9@gmail.com>
2023-05-11 15:41:14 +0530

    seven

---
+++ users demo function.py
@@ -0,0 +1,46 @@
+#function with no arugument & no return value.
+
+def printline():
+    print("*"*30)
+printline()
+print("varsha haresh.")
+
+
+#function with arugument but no return value.
+
+def add(a,b):
+    print("addition:",a+b)
+printline()
+add(10,30)
+printline()
+
+#function with  arugument & with return value.
+
+def sub(a,b):
+    return a-b
+
+printline()
+a=int(input("Enter Value:"))
+b=int(input("Enter Value:"))
+ans=sub(a,b)
+print("Subtraction : ",ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
+    
